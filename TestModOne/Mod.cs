ddad98ddddcf8cef0a6e448ce14bca93f2588ea0
Morgan Touverey Quilling <morgan@mtq.io>
2024-06-21 23:30:18 +0200

    Initial commit

---
+++ TestModOne/Mod.cs
@@ -0,0 +1,34 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using Game.SceneFlow;
+using Colossal.IO.AssetDatabase;
+
+namespace TestModOne {
+    public class Mod : IMod {
+        public static ILog log = LogManager.GetLogger($"{nameof(TestModOne)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private Setting m_Setting;
+
+        public void OnLoad(UpdateSystem updateSystem) {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+            AssetDatabase.global.LoadSettings(nameof(TestModOne), m_Setting, new Setting(this));
+        }
+
+        public void OnDispose() {
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null) {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+    }
+}
